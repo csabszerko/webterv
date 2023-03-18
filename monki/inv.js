0748379c1dc4c8c90271026e65fc2c19eb752f53
csabszerko <kiss.csaba.david@gmail.com>
2023-03-18 21:14:03 +0100

    stat tab demo + adjustments

--- monki/inv.js
+++ monki/inv.js
@@ -4,15 +4,15 @@ const clearMonke = document.getElementById("majomButton1");
 
 var storedInv = localStorage.getItem('inventory');
 
-const vezNev = ["Gelencsér", "Lakatos", "Bagacsi", "Lopodi", "Raffai", "Zsoldos", "Miksi", "Kab", "Pap","Budai","Foltos","Lantos","Lapp","Mondok","Pupák","Sonkolyos","Csápi","Kalapos","Galamb","Rozs","Mészöly"];
-const kerNev = ["Tamás", "Ulrich", "Herold", "Botond", "Alajos", "Mór", "Szabi", "Leopold","Balambér","Gilbert","Taksony","Habakuk","Tódor","Tóbiás","Juszuf","Barni","Kázmér","Oszvald","Tihamér","عبد المجيد"];
+const vezNev = ["Kapolcs","Illés","Virág","Pufatag","Gelencsér", "Lakatos", "Bagacsi", "Lopodi", "Raffai", "Zsoldos", "Miksi", "Kab", "Pap","Budai","Foltos","Lantos","Lapp","Mondok","Pupák","Sonkolyos","Csápi","Kalapos","Galamb","Rozs","Mészöly"];
+const kerNev = ["Illés","Ármin","Krisztián","Tamás", "Ulrich", "Herold", "Botond", "Alajos", "Mór", "Szabi", "Leopold","Balambér","Gilbert","Taksony","Habakuk","Tódor","Tóbiás","Juszuf","Barni","Kázmér","Oszvald","Tihamér","عبد المجيد"];
 
 function kerekMajmot(){
     const li = document.createElement("li");
     const img = document.createElement("img");
     img.width = 100;
     img.height=100;
-    img.src="majmok/majom" + (Math.floor(Math.random() * 21) +1)+ ".jpg";
+    img.src="majmok/majom" + (Math.floor(Math.random() * 20) +1)+ ".jpg";
 
     var nev = (vezNev[Math.floor(Math.random() * vezNev.length)]+" "+kerNev[Math.floor(Math.random() * kerNev.length)]);
 
