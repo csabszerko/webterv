c4fa9e14117f6cfdc84dfc689f2a78052c3ce973
csabszerko <kiss.csaba.david@gmail.com>
2023-03-19 18:09:55 +0100

    kibaszott sok ujitas

--- monki/javascript/inv.js
+++ monki/javascript/inv.js
@@ -4,17 +4,24 @@ const clearMonke = document.getElementById("clearMajomButton");
 
 var storedInv = localStorage.getItem('inventory');
 
-const vezNev = ["Kapolcs","Illés","Virág","Pufatag","Gelencsér", "Lakatos", "Bagacsi", "Lopodi", "Raffai", "Zsoldos", "Miksi", "Kab", "Pap","Budai","Foltos","Lantos","Lapp","Mondok","Pupák","Sonkolyos","Csápi","Kalapos","Galamb","Rozs","Mészöly"];
-const kerNev = ["Illés","Ármin","Krisztián","Tamás", "Ulrich", "Herold", "Botond", "Alajos", "Mór", "Szabi", "Leopold","Balambér","Gilbert","Taksony","Habakuk","Tódor","Tóbiás","Juszuf","Barni","Kázmér","Oszvald","Tihamér","عبد المجيد"];
+const vezNev = ["Lápi","Kapolcs","Illés","Virág","Gelencsér", "Lakatos", "Bagacsi", "Lopodi", "Raffai", "Zsoldos", "Miksi", "Kab", "Pap","Budai","Foltos","Lantos","Lapp","Mondok","Pupák","Sonkolyos","Csápi","Kalapos","Galamb","Rozs","Mészöly"];
+const kerNev = ["Huba","Kázmér","Illés","Ármin","Krisztián","Tamás", "Ulrich", "Herold", "Botond", "Alajos", "Mór", "Szabi", "Leopold","Balambér","Gilbert","Taksony","Habakuk","Tódor","Tóbiás","Juszuf","Barni","Kázmér","Oszvald","Tihamér","عبد المجيد"];
 
-function kerekMajmot(){
+function kerekMajmot(ujKepId){
+
     const li = document.createElement("li");
     const img = document.createElement("img");
     img.width = 100;
     img.height=100;
-    img.src="../majmok/majom" + (Math.floor(Math.random() * 20) +1)+ ".jpg";
+    if(ujKepId==0)
+    {
+        img.src="../majmok/majom" + (Math.floor(Math.random() * 20) +1)+ ".jpg";
+    }else{
+        img.src="../majmok/majom" + (ujKepId)+ ".jpg";
+    }
 
     var nev = (vezNev[Math.floor(Math.random() * vezNev.length)]+" "+kerNev[Math.floor(Math.random() * kerNev.length)]);
+    // ujNev=nev;
 
     li.appendChild(img);
     li.appendChild(document.createTextNode(nev));
@@ -22,6 +29,8 @@ function kerekMajmot(){
     myList.prepend(li);
 
     saveToLocal();
+    //nem mindig letezik ez a fuggveny;
+    setName(nev);
 }
 
 const saveToLocal = ()=>
@@ -44,7 +53,7 @@ if(addMonke)
 {
     addMonke.addEventListener("click", function(e) {
         // addMonke.innerText="meg";
-        kerekMajmot();
+        kerekMajmot(0);
         
     });
 }
---
+++ monki/javascript/wheel.js
@@ -0,0 +1,59 @@
+let nyertNev ="ezt a majmot nyerted";
+var kepId = 1;
+
+(function(){
+    const wheel = document.getElementById("kerek");
+    const spinBtn = document.getElementById("spinButton");
+    // const resultDiv = document.querySelector(".result");
+    const resultContainerDiv = document.querySelector(".resultContainer");
+    const nyeremenyDiv = document.querySelector(".nyeremeny");
+    let deg=0;
+    // let kepId = 1;
+
+    spinBtn.addEventListener("click", ()=> {
+        spinBtn.style.backgroundColor = "coral";
+        spinBtn.style.pointerEvents = "none";
+        deg = Math.floor(Math.random() * 3000 + 3000);
+        wheel.style.transition = `all 10s cubic-bezier(.3,0,0,1)`;
+        wheel.style.transform = `rotate(${deg}deg)`;
+
+        kepId = 20-Math.round((deg%360/18));
+        console.log(deg);
+        console.log(deg%360);
+        console.log(kepId);
+        
+        kerekMajmot(kepId);
+    });
+
+    wheel.addEventListener("transitionend", ()=>{
+        spinBtn.style.backgroundColor = "#04AA6D";
+        spinBtn.style.pointerEvents = "auto";
+        wheel.style.transition = "none";
+        const currRot = deg % 360;
+        wheel.style.transform = `rotate(${currRot}deg)`;
+
+        // resultContainerDiv.removeChild(resultDiv);
+
+        empty(resultContainerDiv);
+
+        const resultDiv = document.createElement("div");
+        resultDiv.classList.add("result");
+        resultContainerDiv.appendChild(resultDiv);
+
+        resultDiv.innerHTML = '<img src="../majmok/majom' + kepId + '.jpg">'
+        + '<p>' + nyertNev + ' személyesen' + '</p>';
+        // resultDiv.innerHTML = "xds";
+        nyeremenyDiv.innerHTML="Nyereményed:"
+    });
+
+})();
+
+function setName(name){
+    nyertNev=name;
+}
+
+function empty(element) {
+    while(element.firstElementChild) {
+       element.firstElementChild.remove();
+    }
+  }
