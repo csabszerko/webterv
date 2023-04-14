ea60b90d284e7441b82563384e08ec12b4e3d1c9
csabszerko <kiss.csaba.david@gmail.com>
2023-04-14 14:13:14 +0200

    file iras regibol

--- php/registration.php
+++ php/registration.php
@@ -2,10 +2,21 @@
     include_once($_SERVER["DOCUMENT_ROOT"]."/webterv/php/handyFunctions.php");
     $users = beolvas(); 
 
-    if((isset($_POST["username"]) && trim($_POST["username"]) !== "") && (isset($_POST["password"]) && trim($_POST["password"]) !== ""))
+    if((isset($_POST["username"]) && trim($_POST["username"]) !== "") && (isset($_POST["password"]) && trim($_POST["password"]) !== "") && (isset($_POST["password_confirm"]) && trim($_POST["password_confirm"]) !== ""))
     {
-        
-    }
+        foreach($users as $user) 
+            {
+                if($user->$username == $_POST["username"])
+                {
+                    echo "A felhasználó név foglalt.";
+                    exit();
+                }
+            }
 
+        if($_POST["password_confirm"] == $_POST["password"])
+        {
+            array_push($users, new User($_POST["username"], $_POST["password"], ""));
+        }
+    }
     kiir($users);
 ?>
