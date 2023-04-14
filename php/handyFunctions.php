44e245f5c20ae1906a5e00942252c79857c64914
csabszerko <kiss.csaba.david@gmail.com>
2023-04-14 14:56:54 +0200

    🖕🏼

--- php/handyFunctions.php
+++ php/handyFunctions.php
@@ -7,23 +7,26 @@
 
         function __construct($nev, $jelszo, $kuki)
         {
-            $this->$username = $nev;
-            $this->$password = $jelszo;
-            $this->$cookie = $kuki;
+            $this->username = $nev;
+            $this->password = $jelszo;
+            $this->cookie = $kuki;
         }
     }
 
     function beolvas()
     {
-        if($file = fopen($_SERVER["DOCUMENT_ROOT"]."/webterv/adatok/users.csv", "r") != false)
+        if(($file = fopen($_SERVER["DOCUMENT_ROOT"]."/webterv/adatok/users.csv", "r")) != false)
         {
             //beolvasom a filet egy tombbe
             $users = array();
-            while ($data = fgetcsv($file, 1000, ",") != FALSE) 
+            while (($data = fgetcsv($file, 1000, ",")) !== FALSE) 
             {
-                echo "ez meg mukodik";
-                array_push($users, new User($data[0], $data[1], $data[2]));
-                echo count($data);
+                // echo "ez meg mukodik";
+                if(count($data) == 3)
+                {
+                    array_push($users, new User($data[0], $data[1], $data[2]));
+                }
+                // echo count($data);
             }
             fclose($file);
             return $users;
@@ -32,13 +35,14 @@
 
     function kiir($users)
     {
-        if($file = fopen($_SERVER["DOCUMENT_ROOT"]."/webterv/adatok/users.csv", "w") != false)
+        if(($file = fopen($_SERVER["DOCUMENT_ROOT"]."/webterv/adatok/users.csv", "w")) != false)
         {
+            // echo "ez meg fasz";
             //kiirom a filet a tombbol
 
             foreach($users as $user) 
             {
-                fwrite($file, $user->$username . "," . $user->$password . "," . $user->$cookie . "\n");
+                fwrite($file, $user->username . "," . $user->password . "," . $user->cookie . "\n");
             }
             fclose($file);
         }
--- php/registration.php
+++ php/registration.php
@@ -1,22 +1,26 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     include_once($_SERVER["DOCUMENT_ROOT"]."/webterv/php/handyFunctions.php");
     $users = beolvas(); 
-    echo "ez meg betolt";
     if((isset($_POST["username"]) && trim($_POST["username"]) !== "") && (isset($_POST["password"]) && trim($_POST["password"]) !== "") && (isset($_POST["password_confirm"]) && trim($_POST["password_confirm"]) !== ""))
     {
         foreach($users as $user) 
             {
-                if($user->$username == $_POST["username"])
+                if($user->username == $_POST["username"])
                 {
-                    echo "A felhasználó név foglalt.";
+                    echo "A felhasználó név foglalt. 🖕🏼";
                     exit();
                 }
             }
 
         if($_POST["password_confirm"] == $_POST["password"])
         {
-            array_push($users, new User($_POST["username"], $_POST["password"], ""));
+            array_push($users, new User($_POST["username"], $_POST["password"], "inshallah"));
         }
+        else echo "Nem egyezik a 2 jelszó. 🖕🏼";
     }
     kiir($users);
+    // echo "ez meg betolt";
 ?>
