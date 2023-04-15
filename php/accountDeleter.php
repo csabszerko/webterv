690cb928cd9b8b079ddf4983a97aeb0ccb2ab288
csabszerko <kiss.csaba.david@gmail.com>
2023-04-16 01:44:46 +0200

    torles

---
+++ php/accountDeleter.php
@@ -0,0 +1,34 @@
+<?php
+include_once($_SERVER["DOCUMENT_ROOT"]."/webterv/php/handyFunctions.php");
+include_once($_SERVER["DOCUMENT_ROOT"]."/webterv/php/inventoryManager.php");
+
+$counter = 0;
+$users = beolvas();
+if($_POST["toroljemAzAccot"])
+{
+    foreach ($users as $user) 
+    {
+        $counter++;
+        if($user->username == getUserName())
+        {
+            $user->username = "";
+            $user->password = "";
+            $user->cookie = "";
+            // unset($user);
+            $inventories = readInventoryFile();
+            foreach($inventories as &$inventory)
+            {
+                if($inventory->username == getUserName())
+                {
+                    
+                    $inventory->username = "";
+                    $inventory->inventory = "";
+                    // $LOADINV = true;
+                }
+            }
+            writeInventoryFile($inventories);
+            kiir($users);
+        }
+    }
+}
+?>
